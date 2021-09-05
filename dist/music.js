99a84bef8f7f108793533284935fb1571cb91bef
Jying-Steml <1904347928@qq.com>
2021-09-05 17:17:33 +0800

    Site updated: 2021-09-05 17:17:33

--- dist/music.js
+++ dist/music.js
@@ -8,7 +8,12 @@ const ap = new APlayer({
         artist: '周深',
         url: 'http://music.163.com/song/media/outer/url?id=1371939273.mp3',
         cover: 'http://p1.music.126.net/1YrCPOBV314i-mTtlDg8mQ==/109951164148664637.jpg?param=130y130',
+    },
+    {
+        name: "Monsters (Live)",
+        artist: '周深',
+        url: 'http://music.163.com/song/media/outer/url?id=1428598981.mp3',
+        cover: 'http://p1.music.126.net/8CBeG0BF2C0dPiA1FDflGQ==/109951164773621591.jpg?param=130y130',
     }
-
     ]
 });
