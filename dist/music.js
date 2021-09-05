36113fbf4ca8764e256b66aab26b5e0da619278c
Jying-Steml <1904347928@qq.com>
2021-09-05 17:04:16 +0800

    Site updated: 2021-09-05 17:04:15

---
+++ dist/music.js
@@ -0,0 +1,14 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+    {
+        name: "亲爱的旅人啊",
+        artist: '周深',
+        url: 'http://music.163.com/song/media/outer/url?id=1371939273.mp3',
+        cover: 'http://p1.music.126.net/1YrCPOBV314i-mTtlDg8mQ==/109951164148664637.jpg?param=130y130',
+    }
+
+    ]
+});
